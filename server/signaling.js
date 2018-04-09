51a61e00ecd14ea4889fa4c4f90bdff977fc64b2
Shunsuke Hiratsuka <hiratsukashu@indival.co.jp>
2018-04-09 12:02:16 +0900

    動画保存できた

--- server/signaling.js
+++ server/signaling.js
@@ -1,5 +1,5 @@
 "use strict";
- 
+var fs = require('fs'); 
 let WebSocketServer = require('ws').Server;
 let port = 3001;
 let wsServer = new WebSocketServer({ port: port });
@@ -8,14 +8,30 @@ console.log('websocket server start. port=' + port);
 wsServer.on('connection', function(ws) {
   console.log('-- websocket connected --');
   ws.on('message', function(message) {
-    wsServer.clients.forEach(function each(client) {
-      if (isSame(ws, client)) {
-        console.log('- skip sender -');
-      }
-      else {
-        client.send(message);
-      }
+   
+    console.log(message.type);
+    console.log(message);
+
+    fs.writeFile("/tmp/test.webm", message,  "binary",function(err) {
+        if(err) {
+            console.log(err);
+        } else {
+            console.log("The file was saved!");
+        }
     });
+
+    if(message.type === ''){
+        console.log('receive binarydata.');
+    }else{
+        wsServer.clients.forEach(function each(client) {
+            if (isSame(ws, client)) {
+                console.log('- skip sender -');
+            }
+            else {
+                client.send(message);
+            }
+        });
+    }  
   });
 });
  
